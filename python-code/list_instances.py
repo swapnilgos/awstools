abc431325587424f6351d310b2a1694f3308cab6
Swapnil Goswami <goswami.swapnil@gmail.com>
2017-03-09 11:57:46 +0000

    few changes

--- python-code/list_instances.py
+++ python-code/list_instances.py
@@ -1,19 +1,22 @@
-import sys;
-import boto; 
+import boto.ec2; 
+import sys; 
 
-print "\nPython Boto Code to List all Instances in AWS Account\n\n" 
-print "\tUsage: python list_instances.py [all, running, stopped]" 
-
-numb_of_args=len(sys.argv)
+numb_of_args = len(sys.argv)
 
 if numb_of_args <= 1: 
-   print "\n\tThis script expects atleast 1 argument! See usage!" 
-   exit(0)
-else:
-   print ""
- 
-if(sys.argv[1] == "all"):
-  print "start instances"
-else: 
-  print "stop instances" 
+   print "Error... Need to pass region" 
+   exit(0); 
+
+ec2_conn = boto.ec2.connect_to_region(sys.argv[1])
+reservations = ec2_conn.get_all_reservations(); 
+
+print reservations 
+
+inst_list = reservations[0].instances 
+
+ins = inst_list[0]
+
+print ins.instance_type
+print ins.state 
+
 
